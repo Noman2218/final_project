baf7f79ee91ecb372678a8699c9b5858b3e27ea4
M.Noman <hayatgil2218@gmail.com>
2024-03-02 19:13:54 +0500

    updated whole website

--- index.js
+++ index.js
@@ -5,35 +5,15 @@ var mybutton = document.getElementById("goTopBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-
-// Flag to determine scrolling behavior
-var isSlowScroll = true;
-
-// When the user clicks on the button, scroll to the top of the document smoothly
+// When the user clicks on the button, scroll to the top of the document
 function goToTop() {
-  const scrollTop = Math.max(
-    document.documentElement.scrollTop,
-    document.body.scrollTop
-  );
-
-  if (scrollTop > 0) {
-    if (isSlowScroll && scrollTop < 200) { // Threshold for slow scrolling
-      window.scrollTo(0, scrollTop - scrollTop / 8); // Slowly scroll upwards
-    } else {
-      isSlowScroll = false; // Set flag to false for free scrolling
-      window.scrollTo(0, 0); // Scroll freely to the top
-    }
-    window.requestAnimationFrame(goToTop);
-  } else {
-    isSlowScroll = true; // Reset flag for the next scroll
-  }
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-  
